c65ed9ec976c440b46dedc514daf883bba940282
de Vries <VriesA@science.regn.net>
2016-09-27 16:15:50 -0400

    Add ability to set insttoken

--- myElsClient.py
+++ myElsClient.py
@@ -6,10 +6,18 @@ class myElsClient:
 
     # local variables
     __base_url = "http://api.elsevier.com/"
-    
+
+    # constructors
     def __init__(self, apiKey):
         """Instantiates a client with a given API Key."""
         self.apiKey = apiKey
 
+    # configuration functions
+    """Sets an institutional token for customer authentication"""
+    def setInstToken(self, instToken):
+        self.instToken = instToken
+
+    # utility access functions
     def getBaseURL(self):
+        """Returns the base URL currently configured for Elsevier's APIs"""
         return self.__base_url
