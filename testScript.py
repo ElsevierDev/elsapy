2bee2c8d341ecc3494f500d6833f87f6d9ceb1de
ale-de-vries <ale@elsevier.com>
2016-10-26 21:58:32 -0400

    Add comments to testScript.py

--- testScript.py
+++ testScript.py
@@ -9,7 +9,7 @@ conFile.close()
 myCl = elsClient(config['apikey'])
 myCl.inst_token = config['insttoken']
 
-## Initialize and read example author
+## Initialize and read example author, then write to disk
 myAuth = elsAuthor('http://api.elsevier.com/content/author/AUTHOR_ID:7004367821')       ## author with more than 25 docs
 if myAuth.read(myCl):
     print ("myAuth.full_name: ", myAuth.full_name)
@@ -17,7 +17,7 @@ if myAuth.read(myCl):
 else:
     print ("Read author failed.")
 
-## Initialize and read example affiliation
+## Initialize and read example affiliation, then write to disk
 myAff = elsAffil('http://api.elsevier.com/content/affiliation/AFFILIATION_ID:60101411')
 if myAff.read(myCl):
     print ("myAff.name: ", myAff.name)
@@ -26,11 +26,11 @@ else:
     print ("Read affiliation failed.")
 
 
-## Initialize and read example document
+## Initialize and read example document, then write to disk
 myDoc = elsDoc('http://api.elsevier.com/content/abstract/SCOPUS_ID:84872135457')
 if myDoc.read(myCl):
     print ("myDoc.title: ", myDoc.title)
-    myDoc.write()
+    myDoc.write()   
 else:
     print ("Read document failed.")
 
