ff5b7099b654d441f0688c324dee0fab10099d34
ale-de-vries <ale@elsevier.com>
2017-01-18 16:33:05 -0500

    Update documentation

--- elsapy/elsentity.py
+++ elsapy/elsentity.py
@@ -1,7 +1,4 @@
-"""A Python module for use with api.elsevier.com. Its aim is to make life easier
-    for people who are not primarily programmers, but need to interact with
-    publication and citation data from Elsevier products in a programmatic
-    manner (e.g. academic researchers).
+"""The (abstract) base entity module for elsapy. Used by elsprofile, elsdoc.
     Additional resources:
     * https://github.com/ElsevierDev/elsapy
     * https://dev.elsevier.com
