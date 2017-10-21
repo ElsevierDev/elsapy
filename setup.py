47e6ce67dbc78ebe5c2b54b585bb71486dbf6fe8
Ale de Vries <aledevries@gmail.com>
2017-10-20 22:36:46 -0400

    Fix clause in setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     author_email = 'integrationsupport@elsevier.com',
     url = 'https://github.com/ElsevierDev/elsapy',
     license = 'License :: OSI Approved :: BSD License',
-    download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
     keywords = ['elsevier api', 'sciencedirect api', 'scopus api'], # arbitrary keywords
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: BSD 3-clause',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
     ],
     packages = find_packages(exclude=['contrib', 'docs', 'tests*']),
